839d8fa9c84540029bdd013582b98888e4f1aaa0
guest-p2QYAD <guest-p2QYAD@cnat332924>
2016-10-04 15:26:44 -0300

    arrumando a build

--- src/test/java/br/edu/ifrn/findyourpro/FindYourProApplicationIT.java
+++ src/test/java/br/edu/ifrn/findyourpro/FindYourProApplicationIT.java
@@ -23,7 +23,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = ContaApplication.class)
+@SpringBootTest(classes = FindYourProApplication.class)
 public class FindYourProApplicationIT {
 
 	@Test
